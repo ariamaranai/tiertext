8f0445bbd3deeda68c3bb40c01d05614783751af
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-01-31 10:20:45 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,83 +1,79 @@
 d.onclick = async () => {
-  let a = document;
-  let b = a.body;
-  if (d.textContent != "✖") {
-    let cvs = new OffscreenCanvas(2496, 4096);
-    let ctx = cvs.getContext("2d", { alpha: !1 });
-    let p = b.getElementsByTagName("p");
-    let top = 2;
-    let baseline = 56;
-    let maxWidth = 0;
-    let i = 0;
-    ctx.fillRect(0, 0, 2496, 4096);
-    ctx.fillStyle = "#ddd";
-    ctx.font = "600 32px menlo,consolas,monospace,yu gothic,sans-serif";
-    ctx.textBaseline = "middle";
-    while (i < 7) {
-      let left = 74;
-      let rowHeight = 108;
-      let text = p[i].textContent.trim();
-      if (text) {
-        let wordWidth = 0;
-        let wordLeft = left;
-        let j = 0;
-        let textLen = (text = [...text]).length;
-        while (j < textLen) {
-          let c = text[j];
-          if (c != "\n") {
-            let { width, actualBoundingBoxLeft } =  ctx.measureText(c);
-            let cWidth = width + actualBoundingBoxLeft;
-            let right = left + cWidth;
-            right < 2476 ? (
-              ctx.fillText(c, left, baseline),
-              left = right,
-              wordWidth += cWidth
-            ) : (
-              left = 74,
-              baseline += 48,
-              rowHeight += 48,
-              /s/.test(c)
-                ? wordLeft = left
-                : wordWidth < 2486 - (64 + 8 + 2 + 8 + 2)
-                  ? (
-                    ctx.drawImage(cvs, wordLeft, baseline - 16, wordWidth, 32, left, baseline, wordWidth, 32),
-                    ctx.clearRect(wordLeft, baseline - 16, wordWidth, 32),
-                    left += wordWidth
-                  )
-                  : (
-                    ctx.fillText(c, left, baseline),
-                    left += wordWidth
-                  )
-            )
-          } else (
+  let b = document.body,
+      cvs = new OffscreenCanvas(2496, 4096),
+      e = cvs.getContext("2d", { alpha: !1 }),
+      p = b.getElementsByTagName("p"),
+      top = 2,
+      baseline = 56,
+      maxWidth = 0,
+      i = 0;
+  e.fillRect(0, 0, 2496, 4096),
+  e.fillStyle = "#ddd",
+  e.font = "600 32px menlo,consolas,monospace,yu gothic,sans-serif",
+  e.textBaseline = "middle";
+  do {
+    let left = 74,
+        rowHeight = 108,
+        text = p[i].textContent.trim();
+    if (text) {
+      let wordWidth = 0,
+          wordLeft = left,
+          j = 0,
+          textLen = (text = [...text]).length;
+      while (j < textLen) {
+        let c = text[j];
+        if (c != "\n") {
+          let { width, actualBoundingBoxLeft } =  e.measureText(c);
+          let cWidth = width + actualBoundingBoxLeft;
+          let right = left + cWidth;
+          right < 2476 ? (
+            e.fillText(c, left, baseline),
+            left = right,
+            wordWidth += cWidth
+          ) : (
             left = 74,
             baseline += 48,
             rowHeight += 48,
-            wordLeft = left
+            /s/.test(c)
+              ? wordLeft = left
+              : wordWidth < 2486 - (64 + 8 + 2 + 8 + 2)
+                ? (
+                  e.drawImage(cvs, wordLeft, baseline - 16, wordWidth, 32, left, baseline, wordWidth, 32),
+                  e.clearRect(wordLeft, baseline - 16, wordWidth, 32),
+                  left += wordWidth
+                )
+                : (
+                  e.fillText(c, left, baseline),
+                  left += wordWidth
+                )
           )
-          ++j
-        }
-        ctx.save();
-        ctx.fillStyle = ["#80b","#a10","#b70","#a90","#183","#068","#136"][i];
-        ctx.fillRect(2, top, 64, rowHeight);
-        ctx.fillStyle = "#ddd";
-        ctx.font = "600 40px serif";
-        ctx.textAlign = "center";
-        ctx.fillText(["𝐒","𝐀","𝐁","𝐂","𝐃","𝐄","𝐅"][i], 34, top + (rowHeight / 2));
-        top += rowHeight + 2;
-        baseline += 108 + 2;
-        maxWidth < left && (maxWidth = left);
-        ctx.restore();
+        } else (
+          left = 74,
+          baseline += 48,
+          rowHeight += 48,
+          wordLeft = left
+        )
+        ++j
       }
-      ++i;
-   }
-    if (maxWidth) {
-      let result = document.createElement("canvas");
-      result.getContext("bitmaprenderer").transferFromImageBitmap(
-          await createImageBitmap(cvs, 0, 0, result.width = maxWidth + 64, result.height = top)
-        );
-      document.body.appendChild(result).setAttribute("style", "position:fixed;top:54;width:1248;height:4096;opacity:.01");
-      d.textContent = "✖";
+      e.save(),
+      e.fillStyle = ["#80b","#a10","#b70","#a90","#183","#068","#136"][i],
+      e.fillRect(2, top, 64, rowHeight),
+      e.fillStyle = "#ddd",
+      e.font = "600 40px serif",
+      e.textAlign = "center",
+      e.fillText(["𝐒","𝐀","𝐁","𝐂","𝐃","𝐄","𝐅"][i], 34, top + (rowHeight / 2)),
+      top += rowHeight + 2,
+      baseline += 108 + 2,
+      maxWidth < left && (maxWidth = left),
+      e.restore()
     }
-  } else b.lastChild.remove(d.textContent = "⇣DOWNLOAD")
+  } while (++i < 7);
+  maxWidth && (
+    (e = new OffscreenCanvas(maxWidth + 64, top))
+      .getContext("bitmaprenderer").transferFromImageBitmap(
+        await createImageBitmap(cvs, 0, 0, maxWidth + 64, height = top)
+      ),
+    (p = document.createElement("a")).href = URL.createObjectURL(await e.convertToBlob()),
+    p.click(p.download = "")
+  )
 }
