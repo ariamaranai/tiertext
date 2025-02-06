70e8264712b389e328dc81c3934e120bdcfd39b3
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-02-06 13:19:22 +0900

    Add files via upload

--- resources/build.js
+++ resources/build.js
@@ -1,1 +1,13 @@
-
+let css = (await Bun.file("main.css").text())
+  .replace(/\n| {2}|\s(?={)|(?<=\:)\s/g, "");
+let js = (await Bun.file("main.js").text())
+  .replace(/(?<!let)\s/g, "")
+  .replace("(^|)", "(^| )")
+  .replace("asynce", "async e");
+let html = (await Bun.file("main.htm").text())
+  .replaceAll("\n", "")
+  .replace("/*css*/", css);
+Bun.write("../s.js", js);
+Bun.write("../index.htm", html);
+console.log(`html size: ${Bun.gzipSync(Buffer.from(html)).length}`);
+console.log(`js size: ${Bun.gzipSync(Buffer.from(js)).length}`);
